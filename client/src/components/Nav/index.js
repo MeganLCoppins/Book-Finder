f752b74f275595fcd9e07855453003759d1db91a
Megan Coppins <meganlcoppins@gmail.com>
2020-05-10 14:24:16 -0700

    changed bg color and added icon to nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Book from "../../assets/book-icon.png";
 import "./style.css";
 
 function Nav() {
   return (
     <nav className="navBarBg navbar navbar-expand-lg mb-2">
       <ul className="nav">
+        <img src={Book} alt="open book" id="book"></img>
         <li className="nav-item">
           <Link to={"/"} className="nav-link googleBooks" >
             Google Books
