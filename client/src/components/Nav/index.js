7b1842f3c6d836d22cd20a23ded60f66ba4cbfac
Megan Coppins <meganlcoppins@gmail.com>
2020-05-10 13:59:19 -0700

    styled nav component

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -4,20 +4,20 @@ import "./style.css";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg mb-2">
+    <nav className="navBarBg navbar navbar-expand-lg mb-2">
       <ul className="nav">
         <li className="nav-item">
-          <Link to={"/"} className="nav-link" >
+          <Link to={"/"} className="nav-link googleBooks" >
             Google Books
           </Link>
         </li>
         <li className="nav-item">
-          <Link to={"/"} className="nav-link">
+          <Link to={"/"} className="nav-link" id="navLink">
             Search
           </Link>
         </li>
         <li className="nav-item">
-          <Link to={"/saved"} className="nav-link">
+          <Link to={"/saved"} className="nav-link" id="navLink">
             Saved
           </Link>
         </li>
