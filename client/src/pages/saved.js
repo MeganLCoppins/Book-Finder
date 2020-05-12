c8de71a8849ce7927a5c023a6321c6dad74dab55
Megan Coppins <meganlcoppins@gmail.com>
2020-05-12 13:09:44 -0700

    added header to saved page

--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Saved(){
     return(
-        <h1>Saved Books</h1>
+        <h1 style={{background: "rgb(198, 224, 247)", color: "darkslateblue", textAlign: "center", padding: "6%", marginTop: "8%", fontSize: "3.5rem"}}>Saved Books</h1>
     )
 }
 export default Saved;
