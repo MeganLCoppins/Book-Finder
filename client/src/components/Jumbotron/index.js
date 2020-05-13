1f8903e362482190e9f5d8c939f00252c73109eb
Megan Coppins <meganlcoppins@gmail.com>
2020-05-13 15:31:29 -0700

    jumbotron text edit

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -7,7 +7,7 @@ function Jumbotron(){
         <div className="jumbotron jumbotron-fluid" style={{width: "100%", textAlign: "center", color: "darkslateblue", marginBottom: "0px", marginTop: "80px", backgroundColor: "rgb(198, 224, 247)"}}>
           <div className="container">
             <h1 className="display-4" style={{fontWeight: "bold"}}>Google Book Finder</h1>
-            <h3 className="lead" style={{fontSize: "25px"}}>Search for and Save Your Book of Choice</h3>
+            <h3 className="lead" style={{fontSize: "25px"}}>Search For and Save Your Book of Choice</h3>
           </div>
         </div>
     )
