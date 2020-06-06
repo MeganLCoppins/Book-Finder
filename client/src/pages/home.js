bf51c6cd33b720d2d1ce3857ad8ee126fa9961d0
Megan Coppins <meganlcoppins@gmail.com>
2020-06-06 15:23:02 -0700

    refactored home to add react hooks

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,40 +1,32 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import Jumbotron from "../components/Jumbotron/index";
 import Form from "../components/Form/index";
 import Card from "../components/Card/index";
 import API from "../utils/API";
 
-class Home extends Component {
-  state = {
-    books: [],
-    query: "",
-  };
+function Home() {
+  const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState("");
 
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
+  function handleInputChange(e) {
+    const { value } = e.target;
 
-    this.setState({
-      [name]: value,
-    });
-  };
+    setQuery(value);
+  }
 
-  getBooks = () => {
-    API.getBooks(this.state.query)
-      .then((res) => this.setState({ books: res.data }))
-      .catch(() =>
-        this.setState({
-          books: [],
-        })
-      );
-  };
+  function getBooks() {
+    API.getBooks(query)
+      .then((res) => setBooks(res.data))
+      .catch(() => setBooks([]));
+  }
 
-  handleFormSubmit = (e) => {
+  function handleFormSubmit(e) {
     e.preventDefault();
-    this.getBooks();
-  };
+    getBooks();
+  }
 
-  handleSaveBook = (id) => {
-    const book = this.state.books.find((book) => book.id === id);
+  function handleSaveBook(id) {
+    const book = books.find((book) => book.id === id);
     API.saveBook({
       googleId: book.id,
       image: book.volumeInfo.imageLinks.thumbnail,
@@ -43,56 +35,54 @@ class Home extends Component {
       description: book.volumeInfo.description,
       authors: book.volumeInfo.authors,
       link: book.volumeInfo.infoLink,
-    }).then(() => this.getBooks());
-  };
-
-  render() {
-    return (
-      <div>
-        <Jumbotron />
-        <div
-          style={{
-            backgroundColor: "rgba(245, 245, 245, 0.748)",
-            padding: "10%",
-            paddingTop: "3%",
-            marginBottom: "60%",
-          }}
-        >
-          <Form
-            handleInputChange={this.handleInputChange}
-            handleFormSubmit={this.handleFormSubmit}
-            query={this.state.query}
-          />
-          {this.state.books.length ? (
-            <div>
-              {this.state.books.map((book) => (
-                <Card
-                  key={book.id}
-                  image={book.volumeInfo.imageLinks.thumbnail}
-                  title={book.volumeInfo.title}
-                  subtitle={book.volumeInfo.subtitle}
-                  description={book.volumeInfo.description}
-                  authors={book.volumeInfo.authors.join(", ")}
-                  link={book.volumeInfo.infoLink}
-                  Button={() => (
-                    <button
-                      onClick={() => this.handleSaveBook(book.id)}
-                      style={{ background: "darkslateblue", color: "white" }}
-                    >
-                      Save Book
-                    </button>
-                  )}
-                />
-              ))}
-            </div>
-          ) : (
-            <h2 style={{ textAlign: "center", color: "darkslateblue" }}>
-              Search For a Book to Display Results
-            </h2>
-          )}
-        </div>
-      </div>
-    );
+    }).then(() => getBooks());
   }
+
+  return (
+    <div>
+      <Jumbotron />
+      <div
+        style={{
+          backgroundColor: "rgba(245, 245, 245, 0.748)",
+          padding: "10%",
+          paddingTop: "3%",
+          marginBottom: "60%",
+        }}
+      >
+        <Form
+          handleInputChange={handleInputChange}
+          handleFormSubmit={handleFormSubmit}
+          query={query}
+        />
+        {books.length ? (
+          <div>
+            {books.map((book) => (
+              <Card
+                key={book.id}
+                image={book.volumeInfo.imageLinks.thumbnail}
+                title={book.volumeInfo.title}
+                subtitle={book.volumeInfo.subtitle}
+                description={book.volumeInfo.description}
+                authors={book.volumeInfo.authors.join(", ")}
+                link={book.volumeInfo.infoLink}
+                Button={() => (
+                  <button
+                    onClick={() => handleSaveBook(book.id)}
+                    style={{ background: "darkslateblue", color: "white" }}
+                  >
+                    Save Book
+                  </button>
+                )}
+              />
+            ))}
+          </div>
+        ) : (
+          <h2 style={{ textAlign: "center", color: "darkslateblue" }}>
+            Search For a Book to Display Results
+          </h2>
+        )}
+      </div>
+    </div>
+  );
 }
 export default Home;
--- client/src/pages/saved.js
+++ client/src/pages/saved.js
@@ -5,73 +5,72 @@ import API from "../utils/API";
 function Saved() {
   const [books, setBooks] = useState([]);
 
-  useEffect(()=> {
+  useEffect(() => {
     getSaved();
-  }, []) 
-
+  }, []);
 
   function getSaved() {
     API.getSaved()
-      .then((res) => setBooks(res.data ))
+      .then((res) => setBooks(res.data))
       .catch((err) => console.log(err));
-  };
+  }
 
   function handleDeleteBook(id) {
     API.deleteBook(id).then((res) => getSaved());
-  };
+  }
 
-    return (
-      <div>
-        <h1
-          style={{
-            background: "rgba(245, 245, 245, 0.748)",
-            color: "darkslateblue",
-            textAlign: "center",
-            padding: "2%",
-            marginTop: "6%",
-            marginBottom: "0",
-            fontSize: "3.5rem",
-          }}
-        >
-          Saved Books
-        </h1>
-        <div
-          style={{
-            backgroundColor: "rgba(245, 245, 245, 0.748)",
-            padding: "10%",
-            paddingTop: "2%",
-            marginBottom: "15%",
-          }}
-        >
-          {books.length ? (
-            <div>
-              {books.map((book) => (
-                <Card
-                  key={book._id}
-                  image={book.image}
-                  title={book.title}
-                  subtitle={book.subtitle}
-                  description={book.description}
-                  authors={book.authors.join(", ")}
-                  link={book.link}
-                  Button={() => (
-                    <button
-                      onClick={() => handleDeleteBook(book._id)}
-                      style={{ background: "darkslateblue", color: "white" }}
-                    >
-                      Remove Book
-                    </button>
-                  )}
-                />
-              ))}
-            </div>
-          ) : (
-            <h2 style={{ textAlign: "center", color: "white" }}>
-              No Saved Books
-            </h2>
-          )}
-        </div>
+  return (
+    <div>
+      <h1
+        style={{
+          background: "rgba(245, 245, 245, 0.748)",
+          color: "darkslateblue",
+          textAlign: "center",
+          padding: "2%",
+          marginTop: "6%",
+          marginBottom: "0",
+          fontSize: "3.5rem",
+        }}
+      >
+        Saved Books
+      </h1>
+      <div
+        style={{
+          backgroundColor: "rgba(245, 245, 245, 0.748)",
+          padding: "10%",
+          paddingTop: "2%",
+          marginBottom: "15%",
+        }}
+      >
+        {books.length ? (
+          <div>
+            {books.map((book) => (
+              <Card
+                key={book._id}
+                image={book.image}
+                title={book.title}
+                subtitle={book.subtitle}
+                description={book.description}
+                authors={book.authors.join(", ")}
+                link={book.link}
+                Button={() => (
+                  <button
+                    onClick={() => handleDeleteBook(book._id)}
+                    style={{ background: "darkslateblue", color: "white" }}
+                  >
+                    Remove Book
+                  </button>
+                )}
+              />
+            ))}
+          </div>
+        ) : (
+          <h2 style={{ textAlign: "center", color: "white" }}>
+            No Saved Books
+          </h2>
+        )}
       </div>
-    );
+    </div>
+  );
 }
 export default Saved;
